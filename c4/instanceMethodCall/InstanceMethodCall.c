7345229e5a3d3fb557bb56cfb34222b3e5ef026e
Jeremy Garrell <jeremy.garrell@parsons.com>
2021-11-11 16:55:40 -0500

    add instance method call

---
+++ c4/instanceMethodCall/InstanceMethodCall.c
@@ -0,0 +1,16 @@
+#include <jni.h>
+#include <stdio.h>
+#include "InstanceMethodCall.h"
+
+JNIEXPORT void JNICALL 
+Java_InstanceMethodCall_nativeMethod(JNIEnv *env, jobject obj) {
+	jclass cls = (*env)->GetObjectClass(env, obj);
+
+	jmethodID mid = (*env)->GetMethodID(env, cls, "callback", "()V");
+	if (NULL == mid) {
+		return;
+	}
+
+	printf("In C\n");
+	(*env)->CallVoidMethod(env, obj, mid);
+}
